d4d85998cc18bd0aba3242ea7b0f343ba07be71d
Chris Sev <chris.sevilleja@gmail.com>
2021-07-07 10:48:47 -0700

    demo ready

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,15 @@
-import { useState } from "react";
-import { PrismaClient } from "@prisma/client";
-
 export default function Home({ posts }) {
-  console.log(posts);
   return (
     <div className="app">
-      i am the home page
+      <h1>Welcome to our blog!</h1>
+
       {/* loop over the posts */}
       {posts.map((post, index) => (
-        <Post key={index} post={post} />
+        <div className="post" key={index}>
+          <h1>{post.title}</h1>
+          <button onClick={() => like(post.id)}>❤️ {likes}</button>
+        </div>
       ))}
-    </div>
-  );
-}
-
-/**
- * A single post
- */
-function Post({ post }) {
-  const [likes, setLikes] = useState(post.likes);
-
-  async function like(id) {
-    const res = await fetch(`/api/posts/${id}/like`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id }),
-    });
-
-    const data = await res.json();
-    setLikes(data.post.likes);
-  }
-
-  return (
-    <div className="post">
-      <h1>{post.title}</h1>
-      <button onClick={() => like(post.id)}>❤️ {likes}</button>
     </div>
   );
 }
@@ -43,7 +18,7 @@ function Post({ post }) {
  * Go get all posts on the server
  */
 export async function getStaticProps() {
-  const prisma = new PrismaClient();
-  const posts = await prisma.post.findMany({});
+  const posts = [{ title: "My First Post" }];
+
   return { props: { posts } };
 }
